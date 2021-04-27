fccce39928c782eec0992bea3563cdda6460821d
Stephen <stebon24@gmail.com>
2021-04-27 14:19:23 -0700

    cleanup

--- public/js/colorPanel.js
+++ public/js/colorPanel.js
@@ -11,25 +11,29 @@ const colorPanel = {
 		this.colorSelectorDiv.addEventListener('click', () =>
 			this.colorPicker.click()
 		);
+
 		this.colorPicker.addEventListener('input', () =>
 			this.selectNewColor(this.colorPicker.value)
 		);
+
 		this.colorPicker.addEventListener('change', () => {
 			this.updateColorHistory(this.colorPicker.value);
 			this.setColor(this.colorPicker.value);
 		});
 	},
+
 	selectNewColor(color = '#000000') {
 		this.colorPicker.value = color;
 		this.currentColor = color;
 		this.newColorDiv.style.background = color;
 	},
+
 	setColor(color = this.currentColor) {
 		this.currentColorDiv.style.background = color;
 	},
+
 	updateColorHistory(color) {
 		const prevColor = document.createElement('div');
-
 		prevColor.style.background = color;
 		prevColor.classList.add('prevColor');
 		prevColor.addEventListener('click', () => {
@@ -44,21 +48,26 @@ const colorPanel = {
 		this.colorHistoryDiv.appendChild(prevColor);
 		this.colorHistory.push(color);
 	},
+
 	rgbToHex(r, g, b) {
 		return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 	},
+
 	restoreColors(prevColors, currentColor) {
 		_.each(prevColors, (color) => {
 			this.updateColorHistory(color);
 		});
+
 		this.selectNewColor(currentColor);
 		this.setColor(currentColor);
 	},
+
 	reset() {
 		const prevColors = document.querySelectorAll('.prevColor');
 		_.each(prevColors, (color) => {
 			color.remove();
 		});
+
 		this.selectNewColor();
 		this.setColor();
 	},
